fb122b1dcde6a85233f293872b9be39911ee0eaf
konstantinos mazarakis <kostasbusiness@gmail.com>
2022-01-28 14:02:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function like(element){
+    element.innerText = "14 likes";
+    alert("Nija was Liked")
+}
+
+function logOut(element){
+    element.innerText = "Logout";
+}
+
+function remove(element){
+    element.remove();
+}
